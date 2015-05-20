60be0ee3566b03f7ceeec57de36d68d407617571
Kamil Kolodynski <kamilkolodynski@gmail.com>
2015-05-20 21:05:27 +0100

    Added Flickr logic, events, utils and index page

--- src/js/flickr.js
+++ src/js/flickr.js
@@ -1,3 +1,79 @@
-/**
- * Created by kamil on 20/05/15.
- */
+var Flickr = Flickr || {};
+
+(function () {
+    'use strict';
+
+    var pageData,
+        pageContainer;
+
+    Flickr.init = function (container, data) {
+        pageData = data;
+        pageContainer = container;
+        this.render();
+        this.addEvents();
+    };
+
+    Flickr.render = function () {
+        pageData.items = this.loadSelectedFromSessionStorage(pageData.items);
+        console.log(pageData);
+        var div = Flickr.Utils.renderTemplate('flickr_page', pageData, true);
+        pageContainer.innerHTML = "";
+        pageContainer.appendChild(div);
+    };
+
+    Flickr.addEvents = function () {
+
+        if (pageContainer.addEventListener) {
+            pageContainer.addEventListener('click', this.handlePhotoClick, false);
+        }
+        else {
+            pageContainer.attachEvent('onclick', this.handlePhotoClick);
+        }
+    };
+
+    Flickr.handlePhotoClick = function (e) {
+        e = e || window.event;
+        var target = e.target || e.srcElement;
+
+        if (target.className.match(/flickr__image/)) {
+            if (target.className.match(/selected/)) {
+                Flickr.deselectPhoto(target);
+            } else {
+                Flickr.selectPhoto(target);
+            }
+
+        }
+    };
+
+    Flickr.deselectPhoto = function (elem) {
+        //don't have to worry about older browsers as it's only for chrome
+        elem.classList.remove('selected');
+
+        Flickr.Utils.storage.removeFromArray('links', elem.getAttribute('data-link'));
+
+    };
+
+    Flickr.selectPhoto = function (elem) {
+        elem.classList.add('selected');
+
+        Flickr.Utils.storage.addToArray('links', elem.getAttribute('data-link'));
+    };
+
+    Flickr.loadSelectedFromSessionStorage = function (items) {
+        var itemsLength = items.length,
+            sessionItems = Flickr.Utils.storage.getArray('links');
+
+        for (var i=0; i<itemsLength; i++) {
+            var item = items[i],
+                index = sessionItems.indexOf(item.link);
+
+            if (index > -1) {
+                items[i].selected = true;
+            }
+        }
+
+        return items;
+    };
+
+}());
+
--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,3 +1,115 @@
-/**
- * Created by kamil on 20/05/15.
- */
+Flickr.Utils = (function () {
+    'use strict';
+
+    var utils = {};
+
+    utils.renderTemplate = function(name, context, asHtmlElement) {
+        var content = this.trim( Handlebars.templates[name](context) );
+        if(asHtmlElement) {
+            /**
+             We can return the rendered template as an HTMLElement if requested
+             NOTE: this will break if the template has multiple nodes in the root,
+             e.g.
+             for the template: "<div>{{foo}}</div><div>{{bar}}</div>"
+             only the first div, containing foo, will be returned
+             **/
+            var d = document.createElement("div");
+            d.innerHTML = content;
+            return d.firstChild;
+        } else {
+            // otherwise just return the rendered content as a string
+            return content;
+        }
+    };
+
+    utils.trim = function(str) {
+        return str.replace(/^\s+|\s+$/g,'');
+    };
+
+    utils.storage = {
+        set: function (item, value) {
+            sessionStorage.setItem(item, value);
+        },
+        get: function (item) {
+            var value = sessionStorage.getItem(item);
+            return value;
+        },
+        count: function () {
+            var len = sessionStorage.length;
+            return len;
+        },
+        remove: function (item) {
+            sessionStorage.removeItem(item);
+        },
+        empty: function () {
+            sessionStorage.clear();
+        },
+        setArray: function (item, value) {
+            if (value instanceof Array) {
+                value = value.join(',');
+            }
+
+            sessionStorage.setItem(item, value);
+        },
+        getArray: function (item) {
+            var string = sessionStorage.getItem(item);
+
+            if (string) {
+                string = string.split(',');
+            } else {
+                string = [];
+            }
+
+            return string;
+        },
+        addToArray: function (item, value) {
+            var sessionItems = this.getArray(item);
+            if (sessionItems) {
+                var string;
+
+                sessionItems.push(value);
+
+                string = sessionItems.join(',');
+
+                sessionStorage.setItem(item, string);
+            } else {
+                this.setArray(item, value);
+            }
+        },
+        removeFromArray: function (item, value) {
+            var sessionItems = this.getArray(item);
+
+            if (sessionItems) {
+                var index = sessionItems.indexOf(value);
+
+                if (index > -1) {
+                    sessionItems.splice(index, 1);
+                }
+
+                sessionStorage.setItem(item, sessionItems.join(','));
+            }
+        },
+        getArrayLength: function (item) {
+            var sessionItems = this.getArray(item),
+                size = 0;
+
+            if (sessionItems) {
+                size = sessionItems.length;
+            }
+
+            return size;
+        },
+        removeOldestFromArray: function (item) {
+            var sessionItems = this.getArray(item);
+
+            if (sessionItems) {
+                sessionItems.splice(0, 1);
+            }
+
+            sessionStorage.setItem(item, sessionItems.join(','));
+        }
+
+    };
+
+    return utils;
+})();
